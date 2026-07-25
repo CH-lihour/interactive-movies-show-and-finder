8697b1bc67e116c1c1a82419077609838c8ffba8
Hour <239297656+CH-lihour@users.noreply.github.com>
2026-07-25 20:06:25 +0700

    deploy app

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Served from https://ch-lihour.github.io/interactive-movies-show-and-finder/
+  base: '/interactive-movies-show-and-finder/',
   plugins: [react()],
 })
